3e861119c5964d3ab05b084fb03cb9d3680bbfd3
mengtiennn <angela850324@gmail.com>
2021-10-08 17:23:18 +0800

    scss/sass

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="try">123</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+  .try{
+    width: 100px;
+    height: 100px;
+    background-color: orange;
+  }
+</style>
